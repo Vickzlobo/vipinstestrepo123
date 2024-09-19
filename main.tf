edd7009a677b6d5e5de71d28ace7e539c6dc0466
Vickzlobo <vickzlobo@gmail.com>
2024-09-19 18:42:31 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
-i am the first main.tf file created under Main
-i am the new changes under branch1
-i am the new changes
+resource "equinix_fabric_cloud_router" "FCRRouter_in_SG" {
+  name = "FCR_in_SG_Vipin"
+  type = "XF_ROUTER"
+  notifications {
+    type   = "ALL"
+    emails = [var.email]
+  }
+  order {
+    purchase_order_number = var.purchase_order_number
+  }
+  location {
+    metro_code = "SG"
+  }
+  package {
+    code = "STANDARD"
+  }
+  project {
+    project_id = var.project_id
+  }
+  account {
+    account_number = var.account_number
+  }
+}
